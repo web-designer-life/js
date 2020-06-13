05b5857b265c7ee8c2602201538a19625afd72b0
Anton Yulbarysau <io57196657@gmail.com>
2020-06-13 20:09:15 +0300

    fix: lesson06

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 function random(number) {
     return Math.ceil(Math.random() * number);
 }
- 
+
+function game(answer, number) {
+    if  (answer != null) {
+        if (answer == number) {
+            alert('Верно! Это число ' + number + '.');
+        }
+        if (answer != parseInt(answer)) {
+            answer = prompt('Вводить нужно числа.  Какое число я загадал?');
+            game(answer, number);
+        }
+        if (answer < number) {
+            answer = prompt('Мало. Попробуй ещё раз!');
+            game(answer, number);
+        }
+        if (answer > number) {
+            answer = prompt('Много. Попробуй ещё раз!');
+            game(answer, number);
+        }
+    }
+}
+
 let ok = confirm('Угадай число от 1 до 100');
 
 if (ok) {
     let number = random(100);
     let answer = prompt('Какое число я загадал?');
     answer.trim();
-    if  (answer != null) {
-        while (answer != number && answer != null) {
-            if (answer != parseInt(answer)) {
-                answer = prompt('Вводить нужно числа.  Какое число я загадал?');
-                continue;
-            }
-            if (answer < number) {
-                answer = prompt('Мало. Попробуй ещё раз!');
-            }
-            if (answer > number) {
-                answer = prompt('Много. Попробуй ещё раз!');
-            }
-        }   
-        if (answer == number) {
-            alert('Верно! Это число ' + number + '.');
-        }
-    }
+    game(answer, number);
 }
