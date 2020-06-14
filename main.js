21e5880421b62eca0675c0eda95df383cba35ceb
Anton Yulbarysau <io57196657@gmail.com>
2020-06-14 23:24:32 +0300

    lesson07

--- main.js
+++ main.js
@@ -1,32 +1,89 @@
-function random(number) {
-    return Math.ceil(Math.random() * number);
-}
+let isNumber = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
 
-function game(answer, number) {
-    if  (answer != null) {
-        if (answer == number) {
-            alert('Верно! Это число ' + number + '.');
+let money,
+    start = function() {
+        do {
+            money = prompt('Ваш месячный доход?');
         }
-        if (answer != parseInt(answer)) {
-            answer = prompt('Вводить нужно числа.  Какое число я загадал?');
-            game(answer, number);
+        while (!isNumber(money)); 
+    };
+
+start();
+
+let appData = {
+    income: {},
+    addIncome: [],
+    expenses: {},
+    addExpenses: [],
+    deposit: false, 
+    mission: 12345,
+    period: 3,
+    budget: money, 
+    budgetDay: 0,
+    budgetMonth: 0,
+    expensesMonth: 0,
+    asking: function() {
+        let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.');
+        appData.addExpenses = addExpenses.toLowerCase().split(' ');
+        appData.deposit = confirm('Есть ли у вас депозит в банке?');
+        for (let i = 0; i < 2; i++) {
+            let question = prompt('Введите обязательную статью расходов?');
+            let amount;
+            do {
+                amount = +prompt('Во сколько это обойдется?');
+                appData.expenses[question] = amount;
+            } while (!isNumber(amount));
         }
-        if (answer < number) {
-            answer = prompt('Мало. Попробуй ещё раз!');
-            game(answer, number);
+    },
+    getExpensesMonth: function() {
+        let sum = 0;
+        for (let key in appData.expenses) {
+            sum += appData.expenses[key];
         }
-        if (answer > number) {
-            answer = prompt('Много. Попробуй ещё раз!');
-            game(answer, number);
+        return sum;
+    },
+    getBudget: function() {
+        return appData.budget - appData.expensesMonth;
+    },
+    getTargetMonth: function() {
+        return appData.mission / appData.budgetMonth;
+    },
+    getStatusIncome: function() {
+        if (appData.budgetDay >= 1200) {
+            return ('У вас высокий уровень дохода.');
+        }
+        if (appData.budgetDay > 600 && appData.budgetDay < 1200) {
+            return ('У вас средний уровень дохода.');
+        }
+        if (appData.budgetDay >= 0 && appData.budgetDay <= 600 ) {
+            return ('У вас низкий уровень дохода.');
+        }
+        if (appData.budgetDay < 0) {
+            return ('Что то пошло не так.');
         }
     }
-}
+};
 
-let ok = confirm('Угадай число от 1 до 100');
+appData.asking();
+appData.expensesMonth = appData.getExpensesMonth();
+appData.budgetMonth = appData.getBudget();
+appData.budgetDay  = appData.budgetMonth / 30;
 
-if (ok) {
-    let number = random(100);
-    let answer = prompt('Какое число я загадал?');
-    answer.trim();
-    game(answer, number);
+let typeOfMission = function() {
+    if (appData.getTargetMonth() >= 0) {
+        console.log('Цель будет достигнута за ' + Math.ceil(appData.getTargetMonth()) + ' месяцев.');
+    } else {
+        console.log('Цель не будет достигнута.');
+    }
+};
+
+console.log('Расходы за месяц: ' + appData.expensesMonth + '.');
+typeOfMission();
+console.log(appData.getStatusIncome());
+console.log('Наша программа включает в себя данные: ');
+
+for (let key in appData) {
+    console.log('Ключ: ' + key + '. Значение: ' + appData[key] + '.');
 }
